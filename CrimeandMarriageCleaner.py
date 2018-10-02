c8e63fd6ce784fe6603548067122f12e524487e9
jpbananaman <jpbananaman@gmail.com>
2018-10-01 19:38:23 -0500

    Final versions

--- CrimeandMarriageCleaner.py
+++ CrimeandMarriageCleaner.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy
+
 # Load the raw data 
 df = pd.read_csv('marriage_data.csv')
 
@@ -9,8 +10,16 @@ reshape = pd.melt(df, id_vars=['State'], var_name='Year and Type', value_name='R
 #takes the 'Year and Type' column and splits it into 'Year' and 'Relationship'
 reshape['Year'], reshape['Relationship'] = zip(*reshape['Year and Type'].map(lambda x: x.split(' ')))
 
-#drop the 'Year and Type' column from being listed and prints the dataset 
-print(reshape.drop(['Year and Type'], axis=1).to_string())
-
 #produce a csv file of the 'reshape' output
 reshape.drop(['Year and Type'], axis=1).to_csv('MarriageData_output.csv')
+
+# Load the raw data 
+df = pd.read_csv('CrimeTotals.csv')
+
+#reshapes dataframe from wide to long format and renames columns
+reshape = pd.melt(df,id_vars=['Region', 'Division', 'State'],var_name='Year', value_name='Total')
+
+#produce a csv file of the 'reshape' output
+reshape.to_csv('Crime_output.csv')
+
+print("Output files created.")
